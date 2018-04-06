8d0a04ab0481f0036e4b0c1cc68c9ad46e6f7701
Evan Wheeler <anon0mys87@gmail.com>
2018-04-06 10:59:45 -0600

    User story 4 complete

--- spec/features/user_can_see_an_individual_snack_spec.rb
+++ spec/features/user_can_see_an_individual_snack_spec.rb
@@ -26,4 +26,29 @@ feature 'When a user visits a snack show page' do
     expect(page).to have_content(machine_two.location)
     expect(page).to have_content(machine_three.location)
   end
+
+  scenario 'they see each location\'s snack count and average_price' do
+    owner = Owner.create(name: 'Snack Boss')
+    machine_one = owner.machines.create(location: 'Some street somewhere')
+    machine_two = owner.machines.create(location: 'Some street somewhere else')
+    machine_three = owner.machines.create(location: 'A really popular alley')
+    snack_one = Snack.create(name: 'Bag-o-Chips', price: 300)
+    snack_two = Snack.create(name: 'Cand E Bar', price: 200)
+    snack_three = Snack.create(name: 'Soda', price: 500)
+    Machinesnack.create(machine: machine_one, snack: snack_one)
+    Machinesnack.create(machine: machine_two, snack: snack_one)
+    Machinesnack.create(machine: machine_three, snack: snack_one)
+    Machinesnack.create(machine: machine_three, snack: snack_two)
+    Machinesnack.create(machine: machine_three, snack: snack_three)
+
+    visit snack_path(snack_one)
+
+    within 'li:first-child' do
+      expect(page).to have_content("1 kinds of snacks, average price of $3.00")
+    end
+
+    within 'li:last-child' do
+      expect(page).to have_content("3 kinds of snacks, average price of $3.33")
+    end
+  end
 end
