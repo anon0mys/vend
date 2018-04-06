d212b5703e32022118c2384c161846852757f518
Evan Wheeler <anon0mys87@gmail.com>
2018-04-06 10:51:01 -0600

    Added locations to snack show page

--- spec/features/user_can_see_an_individual_snack_spec.rb
+++ spec/features/user_can_see_an_individual_snack_spec.rb
@@ -9,4 +9,21 @@ feature 'When a user visits a snack show page' do
     expect(page).to have_content(snack.name)
     expect(page).to have_content("Snack Price: $#{snack.price / 100}")
   end
+
+  scenario 'they see a list of locations of machines that have the snack' do
+    owner = Owner.create(name: 'Snack Boss')
+    machine_one = owner.machines.create(location: 'Some street somewhere')
+    machine_two = owner.machines.create(location: 'Some street somewhere else')
+    machine_three = owner.machines.create(location: 'A really popular alley')
+    snack = Snack.create(name: 'Bag-o-Chips', price: 300)
+    Machinesnack.create(machine: machine_one, snack: snack)
+    Machinesnack.create(machine: machine_two, snack: snack)
+    Machinesnack.create(machine: machine_three, snack: snack)
+
+    visit snack_path(snack)
+
+    expect(page).to have_content(machine_one.location)
+    expect(page).to have_content(machine_two.location)
+    expect(page).to have_content(machine_three.location)
+  end
 end
